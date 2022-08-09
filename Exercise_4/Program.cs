2d42ee8af28f0ad2d457c7934ca7eb7e82aede9c
Ваше имя <you@example.comgit>
2022-08-09 15:29:26 +0400

    add programm that shows the last number of a three digit number

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+string numberString = Console.ReadLine()!;
+int number1 = int.Parse(numberString);
+
+int number2 = number1 - ((number1 / 10) * 10); // либо короче записать int number2 = number1 %10;
+
+Console.WriteLine(number2);
