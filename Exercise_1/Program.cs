5db50ceaa525a232da5cf7e167827b10b7c4c02e
Ваше имя <you@example.comgit>
2022-08-09 11:31:05 +0400

    add programm square number checker

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Пожалуйста, введите первое целое число и нажмите Enter");
+
+string? numberString1 = Console.ReadLine()!;
+
+int number1 = int.Parse(numberString1);
+
+Console.WriteLine("Пожалуйста, введите второе целое число и нажмите Enter");
+
+string? numberString2 = Console.ReadLine()!;
+
+int number2 = int.Parse(numberString2);
+
+if (number1 == number2 / number1)
+
+{
+   Console.WriteLine("да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
+ 
