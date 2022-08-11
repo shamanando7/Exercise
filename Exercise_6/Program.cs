1673ac1a88e4f438f40d1310507febb44f686598
shaman_ando7 <andranikdashyan@mail.ru>
2022-08-11 16:33:56 +0400

    add Exercise 6

---
+++ Exercise_6/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+Random random = new Random();
+
+int randomNumber = random.Next(000, 1000);
+
+Console.WriteLine("Cлучайное трехзначное число " + randomNumber); // 355
+
+int firstDigit = randomNumber / 10; // 35
+int secondDigit = firstDigit % 10; // 5
+int thirdDigit = randomNumber % 10;
+
+int firstDigit2 = firstDigit / 10;
+int finishNumber = firstDigit2;
+int finishNumber2 = thirdDigit;
+
+
+
+Console.WriteLine("решение " + finishNumber + finishNumber2 );
+
+
+// мое творение.
