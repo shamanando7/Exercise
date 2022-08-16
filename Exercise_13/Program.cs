488c535e771d95674c724054f94ad736c42e129f
shaman_ando7 <andranikdashyan@mail.ru>
2022-08-16 16:50:47 +0400

    add Exercise 13

---
+++ Exercise_13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число");
+string? numberString = Console.ReadLine();
+int x = int.Parse(numberString);
+int index = 1;
+
+
+while (x >=index)
+{
+  var result = Math.Pow(index, 2);
+  index++;
+  Console.WriteLine(result + " ");
+  Console.WriteLine(" ");
+}
+
+if (x < 1)
+  Console.WriteLine("Введены неверные значения");
+
+
+
+
+
+
+
+
+
