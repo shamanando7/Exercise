c79388dd3aca5dbae80c6bc5a6f4fec6cb0d6454
shaman_ando7 <andranikdashyan@mail.ru>
2022-08-17 16:36:17 +0400

    add Exercise 15

---
+++ Exercise_15/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+
+// 7 -> 28 
+// 4 -> 10
+// 8 -> 36
+
+
+int SumFrom1ToA(int a) {                         // здесь функция объявляется
+    int result = 0;
+    
+    for (var i = 0; i <= a; i++)                 // for (инициализатор(счетчик цикла); условие; итератор (шаг) {тело (выполняемые операторы)}
+         result += i;                            // while(условие)
+    return result;                               // {оператор (операторы)}
+}
+
+
+var number = int.Parse (Console.ReadLine()!);     // у нас есть число и сумма чисел до него, считываем (защита от дурака)
+if (number <= 0)
+    return;
+
+var result = SumFrom1ToA(number);                 // функция вызывается здесь
+
+Console.WriteLine(result);
+
+
+
+// class Programm                                       // пример задачи для демонстрации работы цикла
+// {
+//     static void Main(string[]args)
+//     {
+//         for (int i = 0; i <= 10; i++)
+//         Console.WriteLine(i);
+//     }}
+    
+
+
+
